252d488dfc29252107f83a43b765fe0936d00bb3
Hyde Fajar <fajarhide@gmail.com>
2015-09-11 00:17:39 +0700

    Update Installation

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
-
+echo -n 'Install Dependency'
+yum install gcc openssl-devel apr-devel apr-util-devel mysql-server php-mysql -y
+sleep 5;
 echo; echo -n 'Downloading source files Apache...'
-wget -q -O /tmp/ https://raw.githubusercontent.com/fajarhide/apache-custom/master/httpd-custom.tar.gz
+mkdir -p /tmp/apache-custom
 echo -n '.'
-cd /tmp/
+cd /tmp/apache-custom
 echo -n '.'
-/bin/tar xvzf /tmp/httpd-custom.tar.gz
+wget https://raw.githubusercontent.com/fajarhide/apache-custom/master/httpd-custom.tar.gz
 echo -n '.'
-cd /tmp/http-custom
+/bin/tar xvzf /tmp/apache-custom/httpd-custom.tar.gz
+echo -n '.'
+/bin/bash /tmp/apache-custom/httpd-custom/configure --prefix=/usr/local/apache2 --enable-ssl=static --with-mysql --with-ssl=/usr/lib64/openssl/
+sleep 5
+make && make install
+echo -n '.'
+/usr/local/apache2/bin/apachectl -k start
 echo '... Done!'
